153bb06ff3614c8206da30ce6393a20c3e33f931
mostafarawy <58468027+mostafarawy@users.noreply.github.com>
2020-03-21 23:20:04 +0200

    Add files via upload

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,30 +1,24 @@
-input=${sentence[@]}
 
-	all=`awk  -F " " '{print $1}' ${sentence[1]}.metadata`
-	meta=($all)
-	
-	in=`echo $input | awk -F "set" '{print $2}' | awk -F "where" '{print $1}' | awk -F "=" '{print $1}'`
-	inp=($in)
-	
-	
-			for ((j=0 ; j<${#meta[@]};++j))
-				do 
-				if [[ ${inp[0]} == ${meta[$j]}  ]]
-				then
-					m=$((1+$j))
-					inp[0]=$m
-					break ;					
-				fi
-		             done
-		
-var3=${sentence[-1]}
-var2=${sentence[5]}
-n=${inp[0]}
-Nor=`awk   -F , '{if ($1=="'$var3'") {print 'NR'}}'  ${sentence[1]}.csv`
-NN=($Nor)
-for (( i=0; i<${#NN[@]} ; ++i ))
-do
-	NRm=${NN[$i]}
-	var3=`awk  -F , '{if(NR=='$NRm') { print $'$m' }}' ${sentence[1]}.csv`
-	sed -i ''$NRm's/'$var3'/'$var2'/g' ${sentence[1]}.csv
+newval=${sentence[5]}
+
+allin=`awk -F "," '{if(NR==1){for(i=1;i<=NF;i++){if($i=="'${sentence[-3]}'"){set=i}}}else{if($set=="'${sentence[-1]}'") print NR }}' ${sentence[1]}.csv`
+val=($allin)
+
+
+if [[ ${#val[@]} -gt "0" ]]
+then 
+
+for ((j=0; j<${#val[@]};j++))
+do 
+m=${val[$j]}
+old=`awk -F "," '{if(NR==1){for(i=1;i<=NF;i++){if($i=="'${sentence[3]}'"){upd=i}}}else if(NR=='$m'){ print $upd }}' ${sentence[1]}.csv`
+val2=($old)
+oldval=${val2[0]}
+
+sed -i ''$m's/'$oldval'/'$newval'/g' ${sentence[1]}.csv
+
 done
+else
+printf "no elements exists with ${sentence[-3]} "
+
+fi
